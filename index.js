d1d60c55626668e9dd4b1ddd9058e8a31b8359c8
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-06-13 19:53:44 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded',() =>{
+    console.log("The DOM has successfully been loaded")
+    let modifiedParagraphTag = document.getElementById("text")
+    modifiedParagraphTag.textContent = "This is really cool!"
+})
+console.log("Loading the DOM before the event listener")
